bf220c5c2a60ab2d4c9e756afdbd07817773badc
Olegos97 <pointbreak@inbox.ru>
2022-12-20 22:02:48 +0300

    renamed files and folders

--- task_1.10-5/task_1.10-5.go
+++ task_1.10-5/task_1.10-5.go
@@ -5,12 +5,10 @@ import "fmt"
 func main() {
 	var n, c, d int
 	fmt.Scan(&n, &c, &d)
-	var x int
 	for i := 1; i <= n; i++ {
 		if i%c == 0 && i%d != 0 {
-			x = i
+			fmt.Println(i)
 			break
 		}
 	}
-	fmt.Println(x)
 }
